9ae5fada60af8e779f001507d3a4421f8d9dfe51
Coder <coder@example.com>
2026-08-31 08:26:11 -0500

    feat: create database models for users, clinics, warehouses, and requests

---
+++ src/models/Medication.ts
@@ -0,0 +1,40 @@
+import { DataTypes, Model, Optional } from 'sequelize';
+import sequelize from '../config/database';
+
+interface MedicationAttributes {
+  id: number;
+  name: string;
+  description: string;
+}
+
+interface MedicationCreationAttributes extends Optional<MedicationAttributes, 'id'> {}
+
+class Medication extends Model<MedicationAttributes, MedicationCreationAttributes> implements MedicationAttributes {
+  public id!: number;
+  public name!: string;
+  public description!: string;
+}
+
+Medication.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    tableName: 'medications',
+  }
+);
+
+export default Medication;
